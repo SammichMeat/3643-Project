6d97b3c7e4c6048539cac18a1f1631170b6b7373
SammichMeat <adamlt2003@gmail.com>
2024-04-22 15:56:57 -0400

    added correct project files
    files for web ap MVC and Test app NUnit

--- 3643 Calculator/3643 Calculator/CalculatorFunctionsTests.cs
+++ 3643 Calculator/3643 Calculator/CalculatorFunctionsTests.cs
@@ -1,6 +1,6 @@
 namespace _3643_Calculator
 {
-    using NUnit.Framework;
+    
     public class CalculatorFunctionsTests
     {
         
---
+++ 3643 Calculator/TestProject1/UnitTest1.cs
@@ -0,0 +1,23 @@
+using _3643_Calculator.Properties;
+
+namespace TestProject1;
+using NUnit.Framework;
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+        CalculatorEngine calc = new CalculatorEngine();
+    }
+
+    [Test]
+    public void Test1()
+    {
+        //arrange
+        
+        //act
+        
+        //assert
+        Assert.Pass();
+    }
+}
